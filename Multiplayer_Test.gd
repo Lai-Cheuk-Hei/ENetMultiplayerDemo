9b5f9f75239dabd2f154a436e5f72e794ee39d8e
Lai-Cheuk-Hei <anson@innocimda.com>
2024-04-30 11:16:13 +0800

    UI change and disconnect function update

--- Multiplayer_Test.gd
+++ Multiplayer_Test.gd
@@ -3,7 +3,7 @@ extends Control
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
 
-@export var Address = "192.168.32.55"
+@export var Address = "127.0.0.1"
 @export var port = 8910
 
 var player_name : String
@@ -12,44 +12,72 @@ func _ready():
 	multiplayer.peer_connected.connect(peer_connected)
 	multiplayer.peer_disconnected.connect(peer_disconnected)
 	multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connection_failed.connect(connection_failed)
 	if "--server" in OS.get_cmdline_args():
 		hostGame()
 	
 func hostGame():
 	var error = peer.create_server(port)
 	if error != OK:
-		print("cannot host: " + str(error))
-		return
+		print("Cannot host: " + str(error))
+		$ErrorLabel.add_theme_color_override("font_color", Color(1,0,0,1))
+		$ErrorLabel.text = "Cannot host: " + str(error)
+		return false
 	else:
 		print("Now hosting on: " + Address + ":" + str(port))
-	multiplayer.multiplayer_peer = peer
+		$ErrorLabel.add_theme_color_override("font_color", Color(0,1,0,1))
+		$ErrorLabel.text = "Now hosting on port: " + str(port)
+		multiplayer.multiplayer_peer = peer
+		return true
 	#SendPlayerInformation($TextEdit.text, multiplayer.get_unique_id())
 
 func _on_host_pressed():
-	hostGame()
-	SendPlayerInformation($TextEdit.text, multiplayer.get_unique_id())
+	var host_success = hostGame()
+	if host_success:
+		$StartGame.disabled = false
+		SendPlayerInformation($NameEdit.text, multiplayer.get_unique_id())
  
 # this get called on the server and clients
 func peer_connected(id):
 	print("Player Connected " + str(id))
 
+@rpc("any_peer","call_local")
+func CloseServer():
+	print("Server Closed")
+	get_tree().root.get_node("GameScene").queue_free()
+	peer.host.destroy()
+	GameManager.Players = {}
+	self.show()
+
 # this get called on the server and clients
 func peer_disconnected(id):
-	print("Player Disconnected " + str(id))
-	GameManager.Players.erase(id)
-	var players = get_tree().get_nodes_in_group("Player")
-	for i in players:
-		if i.name == str(id):
-			i.queue_free()
+	if id == 1:
+		print("Host Disconnected " + str(id))
+		CloseServer.rpc()
+	else:
+		print("Player Disconnected " + str(id))
+		GameManager.Players.erase(id)
+		var players = get_tree().get_nodes_in_group("Player")
+		for i in players:
+			if i.name == str(id):
+				i.queue_free()
 
 func connected_to_server():
-	print("connected To Sever!")
-	SendPlayerInformation.rpc_id(1, $TextEdit.text, multiplayer.get_unique_id())
+	print("Connected To Sever!")
+	#$ErrorLabel.add_theme_color_override("font_color", Color(0,1,0,1))
+	#$ErrorLabel.text = "Connected To Sever!"
+	SendPlayerInformation.rpc_id(1, $NameEdit.text, multiplayer.get_unique_id())
 
+# called only from clients
+func connection_failed():
+	print("Couldn't Connect")
+	#$ErrorLabel.add_theme_color_override("font_color", Color(1,0,0,1))
+	#$ErrorLabel.text = "Couldn't Connect"
+	
 
 @rpc("any_peer")
 func SendPlayerInformation(name, id):
-	#print(name)
+	# Save player data to GameManager
 	if !GameManager.Players.has(id):
 		GameManager.Players[id] ={
 			"name" : name,
@@ -60,7 +88,8 @@ func SendPlayerInformation(name, id):
 	if multiplayer.is_server():
 		for i in GameManager.Players:
 			SendPlayerInformation.rpc(GameManager.Players[i].name, i)
-			
+	
+	# Updates player list in server
 	for i in $PlayerList/VBoxContainer.get_children():
 		i.queue_free()
 		
@@ -73,21 +102,45 @@ func SendPlayerInformation(name, id):
  
 
 func _on_join_pressed():
-	peer.create_client(Address, port)
+	var error = peer.create_client(Address, port)
+	if error != OK:
+		print("Cannot join " + Address + ": " + str(error))
+		$ErrorLabel.add_theme_color_override("font_color", Color(1,0,0,1))
+		$ErrorLabel.text = "Cannot join " + Address + ": " + str(error)
+		return
+	else:
+		print("Joined game: " + Address + ":" + str(port))
+		$ErrorLabel.add_theme_color_override("font_color", Color(0,1,0,1))
+		$ErrorLabel.text = "Joined game: " + Address + ":" + str(port)
 	multiplayer.multiplayer_peer = peer
  
-
-func _on_text_edit_text_changed(new_text):
-	player_name = $TextEdit.text
-	#print(player_name)
-	pass # Replace with function body.
 
 @rpc("any_peer","call_local")
 func StartGame():
 	var scene = load("res://game_scene.tscn").instantiate()
+	scene.close_server.connect(CloseServer)
 	get_tree().root.add_child(scene)
+	# Updates player list in server
+	for i in $PlayerList/VBoxContainer.get_children():
+		i.queue_free()
+	$ErrorLabel.text = ""
 	self.hide()
 
 func _on_start_game_pressed():
 	StartGame.rpc()
+	pass # Replace with function body.
+
+
+func _on_name_edit_text_changed(new_text):
+	player_name = new_text
+	pass # Replace with function body.
+
+
+func _on_address_edit_text_changed(new_text):
+	Address = new_text
+	pass # Replace with function body.
+
+
+func _on_port_edit_text_changed(new_text):
+	port = int(new_text)
 	pass # Replace with function body.
--- game_scene.gd
+++ game_scene.gd
@@ -3,28 +3,60 @@ extends Control
 @export var PlayerScene : PackedScene
 var rng = RandomNumberGenerator.new()
 
+var player_index
+var server_closed = false
+
+signal close_server
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Only host can close server
+	if multiplayer.get_unique_id() == 1:
+		$CloseServerButton.disabled = false
 	
 	#print(GameManager.Players)
 	for i in GameManager.Players:
-		#print(i)
+		# Player list display
 		var label = Label.new()
 		label.text = GameManager.Players[i].name
 		if(multiplayer.get_unique_id() == GameManager.Players[i].id):
 			label.add_theme_color_override("font_color", Color(1,1,0,1))
+			player_index = i
 		$PlayerList/VBoxContainer.add_child(label)
 		
+		# Init players
 		var currentPlayer = PlayerScene.instantiate()
 		currentPlayer.name = str(GameManager.Players[i].id)
 		currentPlayer.player_name = str(GameManager.Players[i].name)
 		var rng_x = rng.randf_range(128.0, self.size.x - 128.0)
 		var rng_y = rng.randf_range(128.0, self.size.y - 128.0)
 		currentPlayer.position = Vector2(rng_x, rng_y)
-		add_child(currentPlayer)
+		$Players.add_child(currentPlayer)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+@rpc("any_peer","call_local")
+func SetLabel(text, color = Color(1,1,1,1)):
+	$ErrorLabel.text = text
+	$ErrorLabel.add_theme_color_override("font_color", color)
+
+@rpc("any_peer","call_local")
+func CloseServerStatus():
+	server_closed = true
+
+func _on_close_server_button_pressed():
+	$ErrorLabel.text = "Server closed by host.\nPlease wait to return to main screen."
+	SetLabel.rpc("Server closed by host.\nPlease wait to return to main screen.", Color(1,0,0,1))
+	CloseServerStatus.rpc()
+	close_server.emit()
+	pass # Replace with function body.
+
+
+func _on_action_button_pressed():
+	if not server_closed:
+		SetLabel.rpc(GameManager.Players[player_index].name + " acted.")
+	pass # Replace with function body.
